018d84413e39611edd8278ecb2544d392e380ccd
Itsjeet007y1 <jeet.62.jp@gmail.com>
2023-02-13 12:43:48 +0530

    Added code for product and product-category APIs

--- src/main/java/com/ect/ecommercespringboot/dao/ProductRepository.java
+++ src/main/java/com/ect/ecommercespringboot/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.ect.ecommercespringboot.dao;
 
 import com.ect.ecommercespringboot.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
